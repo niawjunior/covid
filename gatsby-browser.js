b90f44cbae9c507341af9c8dbf11b5561b4cd80b
niawjunior <pasupol@dome.cloud>
2020-03-06 20:51:58 +0700

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import "./src/styles/globals.css"
 
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    `เว็บไซต์ มีการ update ` + `ต้องการ refresh หน้าเว็บใหม่หรือไม่?`
+    `เว็บไซต์ มีการ update ต้องการ refresh หน้าเว็บใหม่หรือไม่?`
   )
   if (answer === true) {
     window.location.reload()
