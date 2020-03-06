7687c6c3c7ff93d08aed6251c9c91b2c220afc6b
niawjunior <pasupol@dome.cloud>
2020-03-06 20:50:57 +0700

    update

--- src/components/modal.js
+++ src/components/modal.js
@@ -11,7 +11,7 @@ const ModalView = ({ visible, modalClick }) => {
       onCancel={modalClick}
       footer={[<Button onClick={modalClick}>ปิด</Button>]}
     >
-      <div className="h-64">
+      <div className="h-auto w-auto">
         <Bar />
       </div>
     </Modal>
